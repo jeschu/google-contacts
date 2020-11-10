0579d4674cdc80f3ed3666973b959ce24b2bd3bc
Jens Schumacher <jeschu@ok.de>
2020-11-10 22:44:49 +0100

    working

--- main.js
+++ main.js
@@ -1,25 +1,66 @@
-const { create } = require('domain')
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, session, nativeTheme, Menu } = require('electron')
 
 let win
 
 function createWindow() {
+
+    defaultUserAgent('Chrome')
+
     win = new BrowserWindow({
-        width: 800, height: 600,
+        width: 800, height: 600, show: false,
         webPreferences: { nodeIntegration: true },
         titleBarStyle: 'default'
     })
 
-    win.loadURL('http://contacts.google.com/', { userAgent: 'Chrome' })
+    win.once('ready-to-show', () => win.show())
+
+    // win.loadURL('http://contacts.google.com/', { userAgent: 'Chrome' })
+    win.loadURL('http://contacts.google.com/')
+
+}
+
+function defaultUserAgent(userAgent) {
+
+    session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+        details.requestHeaders['User-Agent'] = userAgent
+        callback({ cancel: false, requestHeaders: details.requestHeaders })
+    })
 
 }
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') { app.quit() }
+    if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) { createWindow() }
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
+
+app.setAboutPanelOptions({
+    applicationName: app.getName(),
+    version: app.getVersion(),
+
+    copyright: 'Jens Schumacher',
+    authors: ['Jens Schumacher'],
+    iconPath: 'assets/app-logo.png',
+})
+
+const isMac = process.platform === 'darwin'
+
+Menu.setApplicationMenu(
+    Menu.buildFromTemplate(
+        [
+            {
+                label: 'about',
+                submenu: [
+                    { role: 'about' },
+                    { type: 'separator' },
+                    { role: 'close' },
+                    { role: 'quit' }
+                ]
+            }
+        ]
+    )
+)
